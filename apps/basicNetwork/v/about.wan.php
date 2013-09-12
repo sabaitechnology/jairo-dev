52394fd67659904aa1ba4c6feee40d412c6a7d5d
Jennifer Ngetich <jen@sabaitechnology.com>
2013-09-12 10:35:00 -0400

    fixed scrollbars

--- apps/basicNetwork/v/about.wan.php
+++ apps/basicNetwork/v/about.wan.php
@@ -3,21 +3,18 @@
 <div class='controlBox'><span class='controlBoxTitle'>About JaiRo</span>
 	<div class='controlBoxContent'>
 	
-		<ul class='nobullets smallText'>
-			<li>	JaiRo is an open source routing project created with the goal becoming the most powerful unified networking platform in the world. Similar in concept to the transition from basic cell phones to smart phones, we are moving routers from a closed source, embedded platform, to an open-source fluid computing platform. The software is created to run on 64-bit x86 hardware and is modular. Installable mods provide endless flexibility and extensibility - driven by you, the developer.
-			</li>
+		<p class='smallText'> JaiRo is an open source routing project created with the goal becoming the most powerful unified networking platform in the world. Similar in concept to the transition from basic cell phones to smart phones, we are moving routers from a closed source, embedded platform, to an open-source fluid computing platform. The software is created to run on 64-bit x86 hardware and is modular. Installable mods provide endless flexibility and extensibility - driven by you, the developer.
+			<br><br>
+			JaiRo is an amazing router platform and is special for several reasons:
 			<br>
-			<li> JaiRo is an amazing router platform and is special for several reasons:
-			</li>
-			<br>
-				<ul>			
-					<li> First, it is a platform, not just a hardware unit. The software will be released under an open source license and based on Ubuntu Linux Server, meaning it will work on many different x86 (64-bit computer grade) hardware structures.
-					</li>
-					<br>
-					<li> Secondly, JaiRo is modular. The open source community will have the opportunity to develop networking modules for phone servers, IP camera streaming, media servers, and more on a platform that will actually support that functionality at the heart of the network. The extensibility opportunity is amazing and until now, the platform just hasn&#39;t existed. Finally, JaiRo brings a level of usability to networking that has never existed before. Projects like setting up a file server are daunting for many users, so they just don&#39;t use the technology. JaiRo is a platform which allows a packaged solution to be installed with one click, just like you would an app on your smartphone. JaiRo has the ability to put power and understanding at the users fingertips. It allows them to relate to the interface in a way that is intuitive.
-					</li>
-				</ul>	
-		</ul>
+			<ul class='smallText'>			
+				<li> First, it is a platform, not just a hardware unit. The software will be released under an open source license and based on Ubuntu Linux Server, meaning it will work on many different x86 (64-bit computer grade) hardware structures.
+				</li>
+				<br>
+				<li> Secondly, JaiRo is modular. The open source community will have the opportunity to develop networking modules for phone servers, IP camera streaming, media servers, and more on a platform that will actually support that functionality at the heart of the network. The extensibility opportunity is amazing and until now, the platform just hasn&#39;t existed. Finally, JaiRo brings a level of usability to networking that has never existed before. Projects like setting up a file server are daunting for many users, so they just don&#39;t use the technology. JaiRo is a platform which allows a packaged solution to be installed with one click, just like you would an app on your smartphone. JaiRo has the ability to put power and understanding at the users fingertips. It allows them to relate to the interface in a way that is intuitive.
+				</li>
+			</ul>	
+		</p>
 
 
 
--- apps/basicNetwork/v/vpn.gateways.php
+++ apps/basicNetwork/v/vpn.gateways.php
@@ -1,12 +1,10 @@
 <div class='pageTitle'>VPN: Gateways</div>
 
-<!-- TODO: 
- set default selection as default dropdown value
--->
+<div class='controlBox'>
+  <span class='controlBoxTitle'>Gateway Setup</span>
 
-<div class='controlBox'><span class='controlBoxTitle'>Gateway Setup</span>
   <div class='controlBoxContent'>
-    <table class='controlTable'>
+     <table class='controlTable'>
     <tbody>
      <tr><td>Select Default</td><td>
       <select id='default_gateway' name='default_gateway' class='radioSwitchElement'>
@@ -18,33 +16,27 @@
      </td></tr>
     </tbody>
     </table>
-    <br>
 
+    <table id='list' class='listTable'></table>
+      <input type='button' value='Save' onclick='saveGateway();'>
+      <input type='button' value='Cancel' onclick='cancelGateway();'>
+      <input type='button' value='Help' onclick='helpGateway();'> <br><br>
+      <span class='smallText'>Each device connected to the network will be displayed in the device list above. For each device, the user has the option of assigning a gateway; Default, Local, VPN, or Accelerator within the device table. <a id="toggleDesc" onclick="toggleExplain();" href="#">(Show Description)</a> </span>
 
-
-     
-  <table id='list' class='listTable'></table>
-  <input type='button' value='Save' onclick='saveGateway();'>
-  <input type='button' value='Cancel' onclick='cancelGateway();'>
-  <input type='button' value='Help' onclick='helpGateway();'> <br><br>
-  <span class='smallText'>Each device connected to the network will be displayed in the device list above. For each device, the user has the option of assigning a gateway; Default, Local, VPN, or Accelerator within the device table. <a id="toggleDesc" onclick="toggleExplain();" href="#">(Show Description)</a> </span>
-  
-  <ul id="description" class="nobullets smallText noshow">
-    <li><b>Default</b>: Any devices not assigned to Local, VPN, or Accelerator in the device list, will use the default as designated in the default assignment function at the top of the page. ('None' automatically assigns traffic to the routers current network state.)</li>
-    <li><b>Local</b>: The internet connection provided by an ISP, i.e. a normal, non-VPN connection.</li>
-    <li><b>VPN</b>: The connection through VPN, as set up on this router (either PPTP or OpenVPN).</li>
-    <li><b>Accelerator</b>: A Sabai VPN Accelerator connection (a secondary device that handles VPN functions in place of the router. <a href="http://www.sabaitechnology.com/VPN-Accelerator-for-fast-VPN-routing-p/acc1st.htm">More Information</a>).</li>
-    <li><br> </li>
-    <li><b>MAC Address*</b>: The 'hardware' address of the device; this usually does not change and is somewhat unique.</li>
-    <li><b>Address*</b>: The assigned Internet Protocol address of the device; this can regularly change, but will be made static if necessary to assign to a gateway.</li>
-    <li><b>Name*</b>: The host name the device reports or the name assigned on the <a href="advanced-static.asp">Static IP page</a>; in the case of devices for which their is no reported hostname, one is assigned of the form i.interface@ipaddress.</li>
-  </ul>
+    <ul id="description" class="nobullets smallText noshow">
+      <li><b>Default</b>: Any devices not assigned to Local, VPN, or Accelerator in the device list, will use the default as designated in the default assignment function at the top of the page. ('None' automatically assigns traffic to the routers current network state.)</li>
+      <li><b>Local</b>: The internet connection provided by an ISP, i.e. a normal, non-VPN connection.</li>
+      <li><b>VPN</b>: The connection through VPN, as set up on this router (either PPTP or OpenVPN).</li>
+      <li><b>Accelerator</b>: A Sabai VPN Accelerator connection (a secondary device that handles VPN functions in place of the router. <a href="http://www.sabaitechnology.com/VPN-Accelerator-for-fast-VPN-routing-p/acc1st.htm">More Information</a>).</li>
+      <li><br> </li>
+      <li><b>MAC Address*</b>: The 'hardware' address of the device; this usually does not change and is somewhat unique.</li>
+      <li><b>Address*</b>: The assigned Internet Protocol address of the device; this can regularly change, but will be made static if necessary to assign to a gateway.</li>
+      <li><b>Name*</b>: The host name the device reports or the name assigned on the <a href="advanced-static.asp">Static IP page</a>; in the case of devices for which their is no reported hostname, one is assigned of the form i.interface@ipaddress.</li>
+    </ul>
 
   </div>
 
 </div>
-
-
 
 <script type='text/ecmascript' src='php/bin.etc.php?q=gateways'></script>
 <script type='text/ecmascript' src='/libs/jquery.dataTables.min.js'></script>
@@ -58,9 +50,9 @@
   'sAjaxDataProp': 'gateway',
   'sAjaxSource': 'php/bin.vpn.gateway.php',
   'aoColumns': [
-   { 'sTitle': 'MAC',		'mData':'mac' },
-   { 'sTitle': 'Address',	'mData':'ip' },
-   { 'sTitle': 'Name',		'mData':'hostname' },
+   { 'sTitle': 'MAC',   'mData':'mac' },
+   { 'sTitle': 'Address', 'mData':'ip' },
+   { 'sTitle': 'Name',    'mData':'hostname' },
    { 'sTitle': 'Gateway', 'mData': 'dlva', 'sClass': 'gatewayDrop'}],
   'fnInitComplete': function(){
        $('.gatewayDrop').editable(function(value, settings){
