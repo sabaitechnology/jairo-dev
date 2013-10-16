40d5fef5c212201cf39e6b36b0571a8545adb46d
Jennifer Ngetich <jen@sabaitechnology.com>
2013-10-16 17:09:24 -0400

    threw in some options content

--- apps/basicNetwork/v/vpn.openvpnserver.php
+++ apps/basicNetwork/v/vpn.openvpnserver.php
@@ -1,7 +1,43 @@
-<div class='pageTitle'>VPN: OpenVPN Server</div>
+<!-- 
+persistent data
+-->
 
-<div class='controlBox'><span class='controlBoxTitle'>OpenVPN Server</span>
-	<div class='controlBoxContent'>
+<link rel="stylesheet" href="/libs/jquery-ui.min.css" />
+<script type='text/ecmascript' src='/libs/jquery-ui.min.js'></script>
 
+<div class='pageTitle'>VPN: Open VPN Server</div>
+
+<br>
+<div id="accordion">
+	<h3>Tunnel Options</h3>
+	<div>
+		IE: Mode, Local Host, Remote Host, etc.
+	</div>
+	<h3>Server Mode</h3>
+	<div>
+		IE: Netmask, Ifconfig, Push, etc.
+	</div>
+	<h3>Client Mode</h3>
+	<div>
+		IE: Client, Pull, Auth-user etc.
+	</div>
+	<h3>Data Channel Encryption Options</h3>
+	<div>
+		IE: Secret File, Auth Alg, Keysize, etc.
 	</div>
 </div>
+
+<script type='text/ecmascript'>
+
+$( document ).ready(function() {
+	
+	$( "#accordion" ).accordion({ heightStyle: "content", active: "false",
+          collapsible: "true" 
+  });
+});
+
+   
+
+
+</script>
+
--- apps/basicNetwork/v/vpn.pptpserver.php
+++ apps/basicNetwork/v/vpn.pptpserver.php
@@ -4,5 +4,39 @@
 <div class='controlBox'><span class='controlBoxTitle'>PPTP Server</span>
 	<div class='controlBoxContent'>
 
+		<table>
+			<tbody>
+				<tr><td>Phone</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Launch PPD?</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Quirks</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Debug</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Sync HDLC?</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Packet Timeout</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Disable Packet Buffering?</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Idle Wait</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Max Echo Wait</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Log String</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Local Bind?</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Log Level</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Test Type</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+				<tr><td>Test Rate</td><td><input id="" name="" class='shortinput'></td>
+				</tr>
+			</tbody>
+		</table>
+			
+
 	</div>
 </div>
