2f2aa80328cd5f87a24677efdd48a0a9c3f868a1
David Thomas <david@sabaitechnology.com>
2013-10-23 10:28:09 -0400

    Fixing Diagnostics page... intermediate step.

--- apps/basicNetwork/v/diagnostics.logs.php
+++ apps/basicNetwork/v/diagnostics.logs.php
@@ -9,98 +9,58 @@
 </style -->
 
 <!-- Hidden tooltip - click ? to show -->
-<div class='inline'>
+<!--
+	Note: commented out for now; can be added back later - David T.
+-->
+<!-- div class='inline'>
  <div id='tooltip'>
-	        <img class="callout" src="img/callout.gif" />
-	      	<a href='#' class='fright xsmallText' onclick='hide();'>Close</a>
-	        <p> 
-	        <b>Display Inline Help: <input id='inlineHelp' type='checkbox' onClick='displayInline();'></input></b> 
-	        <br> 
-	        <b>Manual Page:</b> <a href="?panel=lorem" target="_blank">Logs</a>
-	        <br>
-	        <b>Links:</b> 
-	        <a href="http://www.wikipedia.com" target="_blank">Wiki Page</a>
-	        
-	      	</p>
-</div>
-</div>
+    <img class="callout" src="img/callout.gif" />
+  	<a href='#' class='fright xsmallText' onclick='hide();'>Close</a>
+    <p> 
+    <b>Display Inline Help: <input id='inlineHelp' type='checkbox' onClick='displayInline();'></input></b> 
+    <br> 
+    <b>Manual Page:</b> <a href="?panel=lorem" target="_blank">Logs</a>
+    <br>
+    <b>Links:</b> 
+    <a href="http://www.wikipedia.com" target="_blank">Wiki Page</a>
+ </div>
+</div -->
+<!-- a href="#" id="question" onclick='show();'>
+    <img id='help' src="img/help.png" />
+</a -->
 
-<div class='pageTitle'>Diagnostics: Logs 
-
-	   
-	<a href="#" id="question" onclick='show();'>
-	    <img id='help' src="img/help.png" />
-	</a>
-	
-
-</div>
+<div class='pageTitle'>Diagnostics: Logs</div>
 
 <div class='controlBox'>
-    <span class='controlBoxTitle'>Logs</span>
-     <div class='controlBoxContent'>
-     	<div id='error'></div>
-			<input type='hidden' name='act' id='act' value='all'>
-
-			<table class='tablemenu'>
-			<tbody>
-				<tr>
+	<span class='controlBoxTitle'>Logs</span>
+	<div class='controlBoxContent'>
+		<div id='error'></div>
+		<input type='hidden' name='act' id='act' value='all'>
+		<table class='tablemenu'>
+		<tbody>
+			<tr>
 				<td>
-					<select id='log' name='log'><?php
-					// ** Possible strategy for encapsulation?
-					//
-					//include("php/bin.diagnostics.logs.php?act=list");
-
-					// $logs = glob('/var/log/*.log');
-					// $logs = glob('/var/log/*');
-
-					 $logdir = '/var/log/';
-
-					 $logs = scandir($logdir);
-					 
-					 $logList = array();
-
-					 foreach($logs as $log){
-						// ignore . and ..
-					  if($log=='.' || $log=='..') continue;
-						//if(is_dir($logdir.$log)){}
-						//echo $log .": ". is_dir($logdir.$log) ."\n";
-					 
-					  $logList[]=$log;
-					 }
-					 foreach($logList as $log){
-					 	echo "<option value='". $log ."'>". $log ."</option>\n";
-					 }
-						// var_dump($logs);
-
-						// foreach(preg_replace(array("|/var/log/|","/\.log$/"),'',glob('/var/log/*.log')) as $lf) echo "<option value='". $lf ."'>". $lf ."</option>\n";
-
-					?></select> |
+					<select id='log' name='log'><?php include("php/bin.diagnostics.logs.php"); ?></select> |
 				</td>
 				<td id='linesDiv'>  
-				 <a onclick="checkNum()" class="pointy" href="#">
-				 	View Last 
-				 	<input onclick='return false;' class='shortinput' type="text" name='lines' id='lines' size='5' value='25' />
-				  Lines </a>
+					<a onclick="checkNum()" class="pointy" href="#">
+					 	View Last 
+						<input onclick='return false;' class='shortinput' type="text" name='lines' id='lines' size='5' value='25' />
+						Lines 
+					</a>
 				<td> | <a onclick="getLog('all'); ignoreError()" class="pointy" href='#'>View All</a> | </td>
 				<td><input type="text" id='find' name='find' class='longinput'><input type="button" value="Find" onclick="getLog('find');" id='finder'></td>
-				</tr>
-	<!-- 			<tr class='inlineHelp helpbox'>
-					<td>I am helpful!
-					</td>
-	 -->			</tr>
-			</tbody>
-			</table>
-			<div class='inlineHelp helpbox'>
-				<p>I am helpful!</p>
-			</div>
+			</tr>
+		</tbody>
+		</table>
 
 			
-			<textarea id='logContents' style="width: 90%; height: 30em" readonly>
-			</textarea>
-		
+		<textarea id='logContents' style="width: 90%; height: 30em" readonly></textarea>
 	</div> <!-- end control box content -->
 </div>
-<div><input type='button' id='log' name='log' value='Download Log File' onclick="getLog('all');"></div>
+<div>
+	<input type='button' id='log' name='log' value='Download Log File' onclick="getLog('all');">
+</div>
 
 <script type='text/ecmascript' src='/libs/jquery.jeditable.min.js'></script>
 <script type='text/javascript'>
