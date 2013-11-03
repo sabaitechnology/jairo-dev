2ba3dbb6bfe4b6c747a47783c60eec141e270b13
David Thomas <david@sabaitechnology.com>
2013-11-03 17:39:42 -0500

    Starting to tweak help; will really need a better strategy, as for now I've just broken it.

--- apps/basicNetwork/index.php
+++ apps/basicNetwork/index.php
@@ -80,10 +80,6 @@ function toggleHelpSection() {
 	return false;
 };
 
-$(function(){
-	$( "#helpButton" ).click(toggleHelpSection);
-});
-
 <?php
  $panel = ( array_key_exists('panel',$_REQUEST) ? preg_replace('/[^a-z\d]/i', '', $_REQUEST['panel']) : null );
  $section = ( array_key_exists('section',$_REQUEST) ? preg_replace('/[^a-z\d]/i', '', $_REQUEST['section']) : null );
@@ -93,105 +89,38 @@ $(function(){
  echo "var panel = '$panel'; var section = '$section';\n";
 ?>
 
+$(function(){
+	$('#goToHelp').attr('href', 'http://wiki.jairoproject.com' + location.search);
+	$('#goToWiki').attr('href', 'help.php' + location.search);
+	$( "#helpButton" ).click(toggleHelpSection);
+});
+
 </script>
 </head><body>
 
 <div id='backdrop'>
+	<?php include('menu.php'); ?>
 
- <div id='menuContainer'>
-	<img id='menuHeader' src='img/menuHeader.png'>
-	<ul id='superMenu'>
+	<div id='panelContainer'>
 
-	 <li>
-	<a id='network' class='superMenuLink button'>Network</a>
-	<ul id='networkSubMenu' class='subMenu'>
-	 <li><a id='network_wan' href='?panel=network&section=wan' class='subMenuLink button'>WAN</a></li>
-	 <li><a id='network_lan' href='?panel=network&section=lan' class='subMenuLink button'>LAN</a></li>
-	 <li><a id='network_time' href='?panel=network&section=time' class='subMenuLink button'>Time</a></li>
-	 <li><a id='network_devicelist' href='?panel=network&section=devicelist' class='subMenuLink button'>Device List</a></li>
-	 <li><a id='network_staticips' href='?panel=network&section=staticips' class='subMenuLink button'>Static IPs</a></li>
-	</ul>
-	 </li>
+		<div id='helpArea'>
+			<img id='helpButton' src='img/help.png'>
+			<div id='helpSection' class='ui-widget-content ui-corner-al'>
+		<!-- 		<a href='#' id='closeHelp' class='xsmallText fright'>Close</a> -->
+				Display Inline Help
+				<input name='inlineHelp' id='inlineHelp' type='checkbox' checked='checked'><br><br>
+				<span style='text-decoration: underline'>Links:</span><br>
+				<a id='goToHelp' href='#'>Help Page</a><br>
+				<a id='goToWiki' href='#'>Wiki Page</a>
+			</div>
+		</div>
 
-	 <li>
-	<a id='wireless' class='superMenuLink button'>Wireless</a>
-	<ul id='wirelessSubMenu' class='subMenu'>
-	 <li><a id='wireless_radio' href='?panel=wireless&section=radio' class='subMenuLink button'>Radio</a></li>
-	 <li><a id='wireless_survey' href='?panel=wireless&section=survey' class='subMenuLink button'>Survey</a></li>
-	 <li><a id='wireless_macfilter' href='?panel=wireless&section=macfilter' class='subMenuLink button'>MAC Filter</a></li>
-	</ul>
-	 </li>
-
-	 <li>
-	<a id='vpn' class='superMenuLink button'>VPN</a>
-	<ul id='vpnSubMenu' class='subMenu'>
-	 <li><a id='vpn_pptp' href='?panel=vpn&section=pptp' class='subMenuLink button'>PPTP</a></li>
-	 <li><a id='vpn_openvpn' href='?panel=vpn&section=openvpn' class='subMenuLink button'>OpenVPN</a></li>
-	 <li><a id='vpn_l2tp' href='?panel=vpn&section=l2tp' class='subMenuLink button'>L2TP</a></li>
-	 <li><a id='vpn_ipsec' href='?panel=vpn&section=ipsec' class='subMenuLink button'>IPSEC</a></li>
-	 <li><a id='vpn_pptpserver' href='?panel=vpn&section=pptpserver' class='subMenuLink button'>PPTP Server</a></li>
-	 <li><a id='vpn_openvpnserver' href='?panel=vpn&section=openvpnserver' class='subMenuLink button'>OpenVPN Server</a></li>
-	 <li><a id='vpn_gateways' href='?panel=vpn&section=gateways' class='subMenuLink button'>Gateways</a></li>
-	</ul>
-	 </li>
-
-	 <li>
-	<a id='diagnostics' class='superMenuLink button'>Diagnostics</a>
-	<ul id='diagnosticsSubMenu' class='subMenu'>
-	 <li><a id='diagnostics_ping' href='?panel=diagnostics&section=ping' class='subMenuLink button'>Ping</a></li>
-	 <li><a id='diagnostics_trace' href='?panel=diagnostics&section=trace' class='subMenuLink button'>Trace</a></li>
-	 <li><a id='diagnostics_nslookup' href='?panel=diagnostics&section=nslookup' class='subMenuLink button'>NS Lookup</a></li>
-	 <li><a id='diagnostics_route' href='?panel=diagnostics&section=route' class='subMenuLink button'>Route</a></li>
-	 <li><a id='diagnostics_logs' href='?panel=diagnostics&section=logs' class='subMenuLink button'>Logs</a></li>
-	</ul>
-	 </li>
-
-	 <li>
-	<a id='security' class='superMenuLink button'>Security</a>
-	<ul id='securitySubMenu' class='subMenu'>
-	 <li><a id='security_firewall' href='?panel=security&section=firewall' class='subMenuLink button'>Firewall</a></li>
-	 <li><a id='security_portforwarding' href='?panel=security&section=portforwarding' class='subMenuLink button'>Port Forwarding</a></li>
-	 <li><a id='security_dmz' href='?panel=security&section=dmz' class='subMenuLink button'>DMZ</a></li>
-	 <li><a id='security_conntrack' href='?panel=security&section=conntrack' class='subMenuLink button'>Conntrack</a></li>
-	 <li><a id='security_upnp' href='?panel=security&section=upnp' class='subMenuLink button'>UPnP</a></li>
-	</ul>
-	 </li>
-
-	 <li>
-	<a id='about' href='?panel=about' class='superMenuLink button'>About</a>
-	 </li>
-
-	 <li>
-	<a id='help' href='?panel=help' class='superMenuLink button'>Help</a>
-	 </li>
-
-
-	</ul>
- </div>
-
- <div id='panelContainer'>
-
-<div id='helpArea'>
-	<img id='helpButton' src='img/help.png'>
-	<div id='helpSection' class='ui-widget-content ui-corner-al'>
-<!-- 		<a href='#' id='closeHelp' class='xsmallText fright'>Close</a> -->
-		Display Inline Help
-		<input name='inlineHelp' id='inlineHelp' type='checkbox' checked='checked'><br><br>
-		<span style='text-decoration: underline'>Links:</span><br>
-		<a id='goToHelp' href='#' onclick='doHelp();'>Manual Page</a><br>
-		<a id='goToHelp' href='#' onclick='doWiki();'>Wiki Page</a>
+		<div id='panel'>
+			<form id='fe'>
+			<?php include($page); ?>
+			</form>
+		</div>
 	</div>
-</div>
-
-	<div id='panel'>
-	<form id='fe'>
-
-<?php include($page); ?>
-
-	</form>
-
-	</div>
- </div>
 </div>
 
 </body></html>
