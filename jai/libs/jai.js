7c15e83d7e9e436d2a422298f35006f42d7a8f89
Jennifer Ngetich <jen@sabaitechnology.com>
2013-12-11 15:24:12 -0500

    some silliness

--- jai/libs/jai.js
+++ jai/libs/jai.js
@@ -1,8 +1,35 @@
-function parseJson(jsonIn){ var jsonObj, error=false, txt=[]; if(jsonIn==null||jsonIn==''){ jsonObj=''; }
- else try { jsonObj=JSON.parse(jsonIn); }catch(ex){ error=true; txt=[ex,'\tRaw JSON: "'+jsonIn+'"']; for(var i in ex){ txt.push('\t'+ i +': '+ ex[i]); } }
- return { err: error, msg: (error?txt.join('\n'):jsonObj) } /* tragically, there's nothing we can do */
+function parseJson(jsonIn){
+	var jsonObj, error=false, txt=[];
+	if(jsonIn==null||jsonIn==''){
+		jsonObj='';
+	}else{
+		try {
+			jsonObj=JSON.parse(jsonIn);
+		}catch(ex){
+			error=true;
+			txt=[ex,'\tRaw JSON: "'+jsonIn+'"'];
+			for(var i in ex){
+				txt.push('\t'+ i +': '+ ex[i]);
+			}
+		}
+	}
+	return {
+		err: error,
+		msg: ( error ? txt.join('\n') : jsonObj )
+	} /* tragically, there's nothing we can do */
 }
-function getJson(jsonIn){ if(jsonIn==''){ return ''; }; jsonObj=parseJson(jsonIn); if(jsonObj.err && errors!==false){ errors.push(jsonObj.msg); return ''; }else return jsonObj.msg; }
+function getJson(jsonIn){
+	if(jsonIn==''){
+		return '';
+	};
+	jsonObj=parseJson(jsonIn);
+	if(jsonObj.err && errors!==false){
+		errors.push(jsonObj.msg);
+		return '';
+	}else{
+		return jsonObj.msg;
+	}
+}
 
 function help(){ window.open('http://sabaitechnology.zendesk.com/anonymous_requests/new','Submit a Support Request','height=600,width=800,top=50,left=50').focus(); return false; }
 
