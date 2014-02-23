bbe6e7e3514d905a44671ea17b8949db2df95926
David Thomas <david@sabaitechnology.com>
2014-02-22 19:30:10 -0500

    Simplifying configuration processes; removing extraneous definitions, streamlining callback sequence.

--- node/jai.conf.js
+++ node/jai.conf.js
@@ -8,28 +8,6 @@ module.exports = (function(){
 	var current = null;
 	// this.showQueue = function(){ for(var i=0; i<q.length; i++) console.log("Q["+ i +"]: "+ JSON.stringify( q[i] ) +"\t"+ typeof( q[i].callback ) ); }
 
-	function run(){
-		if( (q.length == 0) || running ) return;
-		running = true;
-		current = q.shift();
-		me[current.type].apply(me, current.args);
-		// switch(current.type){
-		// 	case "get": getConf( current.file, current.key, next ); break;
-		// 	case "set": setConf( current.file, current.key, current.value, next ); break;
-		// }
-	}
-
-	function addOp(operation){
-		q.push(operation);
-		run();
-	}
-
-	function next(data){
-		running = false;
-		current.callback(data);
-		run();
-	}
-
 	function saveFile(file, data, callback){
 		fs.writeFile(confRoot +"/."+ file, JSON.stringify(data, null, "\t"), function(e){
 			if(e) console.log(file +" save error: "+ JSON.stringify(e) +"\n");
@@ -37,70 +15,86 @@ module.exports = (function(){
 		});
 	}
 
-	function _get(file, key, callback){
-		if(!file) return;
-		if(!callback && (typeof(key) == "function")){ callback = key; key = null; }
-		if(!callback) return; // TODO: throw an error?
-		var confFile = confRoot +"/"+ file;
-		var tempFile = confRoot +"/."+ file;
-		if(!fs.existsSync(tempFile) && fs.existsSync(confFile)) fs.createReadStream(confFile).pipe(fs.createWriteStream(tempFile));
-		fs.readFile(tempFile, function(e, data){
-			if(!e){
-				if(data==""){
-					data = {};
-				}else try { data = JSON.parse(data); } catch(e){
-					// TODO: if data is corrupt we ought probably to save the corrupt contents elsewhere and replace the corrupt file.
-					console.log(file + " parse error: "+ JSON.stringify(e) +"\n");
-					data = null;
-				}
-				if(!key){
-					callback(data);
+	var ops = {
+		get: function (file, key, callback){
+			var confFile = confRoot +"/"+ file;
+			var tempFile = confRoot +"/."+ file;
+			if(!fs.existsSync(tempFile) && fs.existsSync(confFile)) fs.createReadStream(confFile).pipe(fs.createWriteStream(tempFile));
+			fs.readFile(tempFile, function(e, data){
+				if(!e){
+					if(data==""){
+						data = {};
+					}else try { data = JSON.parse(data); } catch(e){
+						// TODO: if data is corrupt we ought probably to save the corrupt contents elsewhere and replace the corrupt file.
+						console.log(file + " parse error: "+ JSON.stringify(e) +"\n");
+						data = null;
+					}
+					if(!key){
+						callback(data);
+					}else{
+						var obj = data;
+						key = key.split('.');
+						var len = key.length;
+						for(var i=0; i<(len-1); i++) obj = obj[key[i]];
+						callback(obj[key[len - 1]]);
+					}
 				}else{
-					var obj = data;
-					key = key.split('.');
-					var len = key.length;
-					for(var i=0; i<(len-1); i++){ obj = obj[key[i]]; }	// TODO: while not for?
-					callback(obj[key[len - 1]]);
-				}
-			}else callback({});
-		});
-	}
-
-	function _set(file, key, value, callback){
-		if(value == null) return;
-		if(!key){
-			saveFile(file,value,callback);
-		}else{
-			_get(file, function(data){
-				if(!data){
-					// TODO: Spaceballs, DO SOMETHING! (Probably save value as new file contents.)
-					//	Also probably handle creating new keys...
-					saveFile(file,value,callback);
-				}else{
-					var obj = data;
-					key = key.split('.');
-					var len = key.length;
-					// TODO: handle case where key doesn't exist.
-					for (var i=0; i<(len-1); i++){ obj = obj[key[i]]; }	// TODO: while not for?
-					obj[key[len-1]] = value;
-					saveFile(file,data,callback);
+					callback({});
 				}
 			});
+		},
+		set: function (file, key, value, callback){
+			if(!key){
+				saveFile(file,value,callback);
+			}else{
+				ops.get(file, null, function(data){
+					if(!data) data = {};
+					var obj = data;
+					key = key.split('.');
+					var len = key.length;
+					for(var i=0; i<(len-1); i++){
+						if(!obj[key[i]]) obj[key[i]] = {};
+						obj = obj[key[i]];
+					}
+					if(obj[key[len-1]] == value){ // Don't bother writing file if value isn't going to change.
+						callback(true);
+					}else{
+						obj[key[len-1]] = value;
+						saveFile(file,data,callback);
+					}
+				});
+			}
 		}
 	}
 
-	function etTest(file, key, callback){
-		for(var i=0; i<arguments.length; i++){
-			console.log(i +": "+ arguments[i]);
-		}
+	function next(data){
+		running = false;
+		current.callback(data);
+		current = null;
+		run();
 	}
 
-	this.set = function(file, key, value, callback){
-		addOp({ type: "_set", args: arguments });
+	function run(){
+		if( (q.length == 0) || running ) return;
+		running = true;
+		current = q.shift();
+		ops[current.type].apply(me, current.args);
 	}
 
 	this.get = function(file, key, callback){
-		addOp({ type: "_get", args: arguments });
+		if(!file) return;
+		if(!callback && (typeof(key) == "function")){ callback = key; key = null; }
+		if(!callback) return; // TODO: throw an error?
+		q.push({ type: "get", callback: callback, args: [ file, key, next ] });
+		run();
+	}
+
+	this.set = function(file, key, value, callback){
+		if(!file) return;
+		if(!key) key = "";
+		if(value == null) value = ""; // Test for null, value may legitimately be false.
+		q.push({ type: "set", callback: callback, args: [ file, key, value, next ] });
+		run();
 	}
 
 // var fw = fs.watch('etc', function (event, filename){ console.log("Filename:"+ filename +"\nEvent:\n" + JSON.stringify(event)); });
