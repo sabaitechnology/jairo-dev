3f5d6d24c3d4adb4c6064fa6f9454e902419b067
David Thomas <david@sabaitechnology.com>
2014-02-24 17:04:52 -0500

    EOD Monday, 24th Feb 2014.

--- node/jai.conf.js
+++ node/jai.conf.js
@@ -16,33 +16,56 @@ module.exports = (function(){
 		});
 	}
 
+	function loadFile(file, temp, callback){
+		fs.readFile(confRoot +"/"+ (temp?".":"") + file, function(e, data){
+			if(e || data==""){
+				data = {};
+			}else{
+				try { data = JSON.parse(data); } catch(e){
+					// TODO: if data is corrupt we ought probably to save the corrupt contents elsewhere and replace the corrupt file.
+					console.log(file + " parse error: "+ JSON.stringify(e) +"\n");
+					data = null;
+				}
+			}
+			callback(data);
+		});
+	}
+
 	var ops = {
 		get: function (file, key, callback){
-			var confFile = confRoot +"/"+ file;
-			var tempFile = confRoot +"/."+ file;
-			if(!fs.existsSync(tempFile) && fs.existsSync(confFile)) fs.createReadStream(confFile).pipe(fs.createWriteStream(tempFile));
-			fs.readFile(tempFile, function(e, data){
-				if(!e){
-					if(data==""){
-						data = {};
-					}else try { data = JSON.parse(data); } catch(e){
-						// TODO: if data is corrupt we ought probably to save the corrupt contents elsewhere and replace the corrupt file.
-						console.log(file + " parse error: "+ JSON.stringify(e) +"\n");
-						data = null;
-					}
-					if(!key){
-						callback(data);
-					}else{
-						var obj = data;
-						key = key.split('.');
-						var len = key.length;
-						for(var i=0; i<(len-1); i++) obj = obj[key[i]];
-						callback(obj[key[len - 1]]);
-					}
-				}else{
-					callback({});
-				}
+			loadFile(file, true, function(temp){
+				loadFile(file, false, function(conf){
+					console.log("Temp: "+ JSON.stringify(temp));
+					console.log("Conf: "+ JSON.stringify(conf));
+				});
 			});
+
+			// var confFile = confRoot +"/"+ file;
+			// var tempFile = confRoot +"/."+ file;
+			// if(!fs.existsSync(tempFile) && fs.existsSync(confFile)) fs.createReadStream(confFile).pipe(fs.createWriteStream(tempFile));
+			// fs.readFile(tempFile, function(e, data){
+			// 	if(!e){
+			// 		if(data==""){
+			// 			data = {};
+			// 		}else try { data = JSON.parse(data); } catch(e){
+			// 			// TODO: if data is corrupt we ought probably to save the corrupt contents elsewhere and replace the corrupt file.
+			// 			console.log(file + " parse error: "+ JSON.stringify(e) +"\n");
+			// 			data = null;
+			// 		}
+			// 		if(!key){
+			// 			callback(data);
+			// 		}else{
+			// 			var obj = data;
+			// 			key = key.split('.');
+			// 			var len = key.length;
+			// 			for(var i=0; i<(len-1); i++) obj = obj[key[i]];
+			// 			callback(obj[key[len - 1]]);
+			// 		}
+			// 	}else{
+			// 		callback({});
+			// 	}
+			// });
+
 		},
 		set: function (file, key, value, callback){
 			if(!key){
@@ -65,12 +88,57 @@ module.exports = (function(){
 					}
 				});
 			}
+		},
+		diff: function(file, callback){
+			if(!deepdiff) deepdiff = require("deep-diff");
+			ops.get(file, null, function(temp){
+				var confFile = confRoot +"/"+ file;
+				var conf = (fs.existsSync(confFile) ? JSON.parse(fs.readFileSync(confFile)) : {} );
+				callback(deepdiff.diff(conf, temp), conf, temp);
+			});
+		},
+		discard: function(file, callback){
+
+		},
+		revert: function(file, key, callback){
+			if(!deepdiff) deepdiff = require("deep-diff");
+
+			var confFile = confRoot +"/"+ file;
+			var tempFile = confRoot +"/."+ file;
+			var conf = (fs.existsSync(confFile) ? JSON.parse(fs.readFileSync(confFile)) : {} );
+			var temp = (fs.existsSync(tempFile) ? JSON.parse(fs.readFileSync(tempFile)) : {} );
+
+			deepdiff.observableDiff(temp, conf, function (d){ if(d.path.join(".") == key) deepdiff.applyChange(temp, conf, d); });
+
+
+			// ops.diff(file, function(d, lhs, rhs){
+			// 	d.forEach(function(v, i, t){
+			// 		console.log("v:");
+			// 		console.log(v);
+			// 		var p = v.path.join(".");
+			// 		if(p == key){
+			// 			// console.log(v);
+			// 			console.log(p +" = "+ key);
+			// 			// console.log(key);
+			// 		}
+			// 		// console.log(
+			// 		// 	JSON.stringify(v)
+			// 		// 	 +" "+ JSON.stringify(i)
+			// 		// 	 +" "+ JSON.stringify(t)
+			// 		// );
+			// 	});
+				// for(var i=0; i<d.length; i++){
+				// 	if(d[i].path)
+				// }
+				// console.log(d);
+				return;
+			// });
 		}
 	}
 
-	function next(data){
+	function next(){
 		running = false;
-		current.callback(data);
+		current.callback.apply(me,arguments);
 		current = null;
 		run();
 	}
@@ -98,13 +166,24 @@ module.exports = (function(){
 		run();
 	}
 
-	this.diff = function(file){
-		if(!deepdiff) deepdiff = require("deep-diff");
-		return ( !fs.existsSync(confRoot+"/."+file) ? [] : deepdiff.diff(
-			(fs.existsSync(confRoot+"/"+file) ? JSON.parse(fs.readFileSync(confRoot+"/"+file)) : {} ),
-			JSON.parse(fs.readFileSync(confRoot+"/."+file))
-		));
+	this.diff = function(file, callback){
+		if(!file || !callback) return;
+		q.push({ type: "diff", callback: callback, args: [ file, next ] })
+		run();
 	}
+
+	this.discard = function(file, callback){
+		if(!file) return;
+		q.push({ type: "discard", callback: callback, args: [ file ] })
+		run();
+	}
+
+	this.revert = function(file, key, callback){
+		if(!file) return;
+		q.push({ type: "revert", callback: callback, args: [ file, key ] })
+		run();
+	}
+
 
 // var fw = fs.watch('etc', function (event, filename){ console.log("Filename:"+ filename +"\nEvent:\n" + JSON.stringify(event)); });
 	return this;
--- node/parseetctest.js
+++ node/parseetctest.js
@@ -6,15 +6,19 @@ var jc		= require("./jai.conf.js");
 function showResult(data){ console.log("Res: "+ JSON.stringify(data, null, "\t") ); }
 
 // jc.get("vpnclients", "kitty", showResult);
-// jc.get("vpnclients", "kitty.username", showResult);
-// jc.get("vpnclients", "kitty.server", showResult);
-// jc.get("vpnclients", "kitty.server", showResult);
-
-jc.get("vpnclients", "kitty", showResult);
-jc.set("vpnclients", "kitty.server", "SERVER FOR OPENVPN", showResult);
-jc.get("vpnclients", "kitty", showResult);
-
-
+// jc.set("vpnclients", "kitty.server", "SERVER FOR OPENVPN", showResult);
+// jc.set("vpnclients", "kitty.password", "PASSWORD FOR OPENVPN", showResult);
+// jc.get("vpnclients", "kitty", showResult);
+// jc.diff("vpnclients", showResult);
+// jc.diff("vpnclients", function(d, lhs, rhs){
+// 	console.log("\tDiff:");
+// 	console.log(d);
+// 	console.log("\tLHS:");
+// 	console.log(lhs);
+// 	console.log("\tRHS:");
+// 	console.log(rhs);
+// });
+jc.revert("vpnclients", "kitty.server", showResult);
 // jc.showQueue();
 
 // jc.set("vpnclients", "kitty.server", "SERVER FOR OPENVPN");
